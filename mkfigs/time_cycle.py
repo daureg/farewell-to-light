be10d8f50b70baaa352dcd9df3163f8527fe38c9
Géraud Le Falher <daureg@gmail.com>
2014-04-24 11:03:58 +0300

    add time cluster plots

--- mkfigs/time_cycle.py
+++ mkfigs/time_cycle.py
@@ -56,11 +56,33 @@ def save_checkins_time(db, city=None, chunk=4):
               np.array([np.arange(7*nb_chunks), 100*week]).T,
               fmt='%d\t%.6f', header='time\tfreq', footer='# '+str(nb_checkin))
 
+
+def save_time_cluster(week, shorter):
+    import scipy.io as sio
+    import cities
+    # import VenueFeature as vf
+    nbclass = 3 if week else 5
+    size = 21 if week else (6 if shorter else 8)
+    # pylint: disable=E1101
+    dirname = '../../' + ('times/' if shorter else 'time/')
+    suffix = '_' + ('weekly' if week else 'daily') + '_time.mat'
+    out_name = '{{}}_cluster_{}_{}'.format('week' if week else 'day',
+                                           '4h' if shorter else '3h')
+    # ticks = np.array(vf.named_ticks('mix' if week else 'day',
+    #                                 2 if shorter else 1,
+    #                                 (4 if shorter else 3)))
+    ticks = np.arange(size).astype(int)
+    fmt = '%d\t' + '\t'.join(nbclass*['%.6f', ])
+    for name in cities.SHORT_KEY:
+        clusters = 100*sio.loadmat(dirname + name + suffix)['t'].T
+        save_data(out_name.format(name), np.c_[ticks, clusters], fmt=fmt)
+
 if __name__ == '__main__':
     # pylint: disable=C0103
-    import CommonMongo as cm
-    import arguments
-    args = arguments.city_parser().parse_args()
-    DB, CLIENT = cm.connect_to_db('foursquare', args.host, args.port)
-    # save_checkins_time(DB, args.city)
-    save_checkins_time(DB)
+    save_time_cluster(False, False)
+    # import CommonMongo as cm
+    # import arguments
+    # args = arguments.city_parser().parse_args()
+    # DB, CLIENT = cm.connect_to_db('foursquare', args.host, args.port)
+    # # save_checkins_time(DB, args.city)
+    # save_checkins_time(DB)
