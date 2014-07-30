a3d30904c4222a48179279979b7a563dc3a7c1b3
Géraud Le Falher <daureg@gmail.com>
2014-07-30 14:05:41 +0300

    don't forget to print the last entry

--- sortbib.py
+++ sortbib.py
@@ -1,5 +1,6 @@
 #! /usr/bin/python
 # vim: set fileencoding=utf-8
+"""Maintain consistent order within a Mendeley exported .bib file"""
 if __name__ == '__main__':
     with open('strip.bib') as f:
         bib = f.readlines()
@@ -16,10 +17,9 @@ if __name__ == '__main__':
             res[name] = '\n'.join([name]+save)
             del save[:]
             name = line.strip()
+            print(name)
         elif inbib:
             save.append(line.strip())
-    res[name] = '\n'.join(save)
-    del save[:]
-    name = line.strip()
+    res[name] = '\n'.join([name]+save)
     with open('nstrip.bib', 'w') as f:
         f.write('\n'.join((_[1] for _ in sorted(res.items()))))
